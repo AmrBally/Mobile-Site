2989f5fff380ef2dd73b679ac2ad71fce88b24c1
AmrBally <114779383+AmrBally@users.noreply.github.com>
2023-05-12 05:27:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.getElementById('img');
+let container = document.querySelector('.container');
+
+function change(phone){
+    img.src = phone;
+}
+
+function color(x){
+    container.style.background = x;
+}
